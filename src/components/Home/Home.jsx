7683de3b434019a1d28c15ae7a1b637eb5e0e9f3
ccxtncntus <ccxtncn00@gmail.com>
2024-03-07 17:05:36 +0700

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Xin chào!{" "}
+                Xin chào!!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
